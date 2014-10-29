d6326e059d08215f253aa9595b9f27b2c139c8eb
c2y3dc <ervinchow@gmail.com>
2014-10-29 16:15:49 -0700

    progress in functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,32 @@
 var makeQueue = function(){
   var someInstance = {};
-
+  var counter = 0;
+  var lowest = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    counter++;
+    storage[counter] = value;
   };
 
   someInstance.dequeue = function(){
+
+    debugger;
+    var output = storage[lowest];
+
+    delete storage[lowest];
+
+    lowest++;
+    counter = counter - lowest;
+    if(counter < 0){return counter = 0;}
+    return output;
   };
 
   someInstance.size = function(){
+    return counter;
   };
 
   return someInstance;
