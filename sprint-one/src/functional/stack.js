f374e70be75f619b30593b3e1667901afa7db1e3
c2y3dc <ervinchow@gmail.com>
2014-10-29 15:19:46 -0700

    Functional Stack specs passed

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,29 @@
 var makeStack = function(){
   var someInstance = {};
+  var counter = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[counter] = value;
+    counter++;
+    console.log(storage);
   };
 
   someInstance.pop = function(){
+    var test =  storage[counter-1];
+    delete storage[counter-1];
+    counter--;
+    if(counter < 0){
+      counter = 0;
+    }
+      return test;
   };
 
   someInstance.size = function(){
+    return counter;
   };
 
   return someInstance;
