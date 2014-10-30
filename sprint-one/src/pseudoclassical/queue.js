c7b23ae3fc6b751b31a0af208b89066ca8e21172
c2y3dc <ervinchow@gmail.com>
2014-10-30 16:08:40 -0700

    pseudoclassical specs passed

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.counter = 0;
+  this.lowest = 0;
+  this.highest = 0;
+  this.storage = {};
+};
+
+Queue.prototype.enqueue = function(value){
+  this.storage[this.highest] = value;
+  this.counter++;
+  this.highest++;
+};
+
+Queue.prototype.dequeue = function(){
+  if(this.counter > 0){
+  var result = this.storage[this.lowest];
+  delete this.storage[this.lowest];
+  this.counter--;
+  this.lowest++;
+  return result;
+  }
+};
+
+Queue.prototype.size = function(){
+  return this.counter;
 };
 
 
