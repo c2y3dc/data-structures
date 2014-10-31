29eaa4a4f24a4c5e427cbcbb068ab92f1f9ecbef
c2y3dc <ervinchow@gmail.com>
2014-10-31 16:42:06 -0700

    set spec progress

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,36 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  //if(typeof item === 'String'){
+    this._storage.push(item);
+    console.log(this._storage);
+  //}
 };
 
 setPrototype.contains = function(item){
+  for(var i = 0; i < this._storage.length; i++){
+
+    if(this._storage[i] === item){
+      //console.log(this._storage[i], item, i);
+      return true;
+    }else{
+      return false;
+    }
+  }
 };
 
 setPrototype.remove = function(item){
+  for(var i = 0; i < this._storage.length; i++){
+    if(this._storage[i] === item){
+      delete this._storage[i];
+    }
+  }
 };
 
 /*
