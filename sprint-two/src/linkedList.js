2db8bf795f0826c5c270719cf17ea7dd20e5e39c
c2y3dc <ervinchow@gmail.com>
2014-10-31 14:39:35 -0700

    linkedlist progress

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,49 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = makeNode(value);
+    if(list.head = null){
+      list.head = newNode;
+    }else{
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
+
+
+    /*
+    var aNode = makeNode(value);
+    if(list.head === null){
+    list.head = aNode;
+    }else{
+      aNode.next = aNode;
+      console.log("!",aNode);
+    }
+    list.tail = aNode;
+    */
   };
 
   list.removeHead = function(){
+    /*
+    var removedHead = list.head.value;
+    delete list.head;
+    list.head =  list.tail;
+    return removedHead;
+    */
   };
 
   list.contains = function(target){
+    /*
+    //while .next !null
+    for(var node in list){
+      if(list[node].value === target){
+        //console.log(list[node].value);
+        return true;
+      }else{
+        //console.log(list[node].value);
+        return false;
+      }
+    }
+    */
   };
 
   return list;
