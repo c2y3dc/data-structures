07cebccf78f479a93eb6b1ba30fd1ad11b7cafda
c2y3dc <ervinchow@gmail.com>
2014-10-31 20:03:56 -0700

    tree progress

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,21 +1,27 @@
 var makeTree = function(value){
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
-
+  if(this.children.length === 0){
+    this.children.push(makeTree(value));
+  }
 };
 
 treeMethods.contains = function(target){
-
+  var result = false;
+  for(var i =0; i < this.children.length; i++){
+    if(this.children[i].value === target){
+      result = true;
+    }
+  }
+  return result;
 };
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
